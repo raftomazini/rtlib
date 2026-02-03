9f0581491c561f6b6c2f2c35d07fe7d6dbc3e749
raftomazini <raftomazini@gmail.com>
2026-02-02 21:21:17 -0300

    Version adjustment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rtlib",
-    version="0.1.0",
+    version="0.1.1",
     description="Biblioteca de utilitarios (Email, OAuth2)",
     author="Rodrigo Tomazini",
     # Encontra automaticamente o pacote 'rtlib' e subpacotes
